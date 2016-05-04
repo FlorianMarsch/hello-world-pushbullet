6fda6b4c66faedeb85fe96319bb4170d971782f9
Florian <dekontaminator@googlemail.com>
2016-05-04 20:58:52 +0200

    proof of concept to push notifications via pushbullet

---
+++ hello-world-pushbullet/src/test/java/com/fat_chicks/hello_world_pushbullet/PushBulletApiTest.java
@@ -0,0 +1,43 @@
+package com.fat_chicks.hello_world_pushbullet;
+import java.io.IOException;
+
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Test;
+
+public class PushBulletApiTest {
+
+
+	@Test
+	public void test() throws ClientProtocolException, IOException, JSONException {
+		String api_key = System.getenv("PUSHBULLET_ACCESS_TOKEN");
+
+		CredentialsProvider credsProvider = new BasicCredentialsProvider();
+		credsProvider.setCredentials(new AuthScope("api.pushbullet.com", 443),
+				new UsernamePasswordCredentials(api_key, null));
+		CloseableHttpClient client = HttpClients.custom().setDefaultCredentialsProvider(credsProvider).build();
+
+		HttpPost httppost = new HttpPost("https://api.pushbullet.com/v2/pushes");
+		httppost.setHeader("Content-Type", "application/json");
+
+		JSONObject message = new JSONObject();
+		message.put("type", "note");
+		message.put("body", "Hello World :)");
+		message.put("channel_tag", System.getenv("PUSHBULLET_CHANNEL_TAG"));
+
+		httppost.setEntity(new StringEntity(message.toString()));
+
+		client.execute(httppost);
+
+	}
+
+}
